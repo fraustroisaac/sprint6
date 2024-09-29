ea12b251803847508dfd65b22ebeea68740ce6b1
Isaac Fraustro <fraustroisaac@gmail.com>
2024-09-29 12:45:16 -0600

    Se envían los archivos por primera vez, incluyendo config.toml

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sprint6
 Proyecto sprint6 tripleten
+
+Proyecto de aplicación WEB que te permitirá:
+
+Dar click en 2 botones distintos, de pendiendo de la necesidad podrás ver 1 gráfico por cada botón en donde se analiza mediante gráficos el comportamiento de los precios de los autos según la columna "odómeter"
---
+++ sprint6.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+        
+car_data = pd.read_csv('notebooks/vehicles_us.csv') # leer los datos
+
+hist_button = st.button('Construir histograma') # crear un botón
+disp_button = st.button('Diagrama de dispersión odometer vs price')
+        
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+     
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if disp_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de dispersión de odometer vs price')
+            
+    # crear un histograma
+    fig2 = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    st.plotly_chart(fig2) # crear gráfico de dispersión
